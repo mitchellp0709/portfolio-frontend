92abfd85b12bcbbad99ef297be5d27880b0776c7
Mitchell Paoletti <mitchellp0709@gmail.com>
2021-11-17 15:00:37 -0500

    fixed nav

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import { renderMatches } from "react-router";
 function Home(props) {
   return (
     <>
-      <h1 className="name">Personal Portfolio of J. Mitchell Paoletti</h1>
+      <h1 className="name">Personal Portfolio of J. Mitchell Paoletti</h1> <br/><br/>
       <h3 className="hook">
         Thank you for visiting my page! Please feel free to browse to learn more
         about me.
@@ -12,6 +12,7 @@ function Home(props) {
       <p className="about">
         I created a portfolio to showcase my frontend and backend abilities. This site itself was created using React states & routes for the front end pages. I created my own backend for all additional information, which I deployed to Heroku and all data is fetched there using API calls. 
       </p>
+      <p className="about">I will be continuously updating this site with new projects, updates to existing projects, and any/all other information so check back frequently!</p>
     </>
   );
 }
